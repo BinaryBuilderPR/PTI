7e43316573f66848e9c959dfe3db6787b16e76f5
Piyush Ranjan <devevloper.piyushranjan@gmail.com>
2024-06-09 20:39:52 +0530

    clean unnecessary files

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,3 @@
 /*!
-* Start Bootstrap - Coming Soon v6.0.7 (https://startbootstrap.com/theme/coming-soon)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-coming-soon/blob/master/LICENSE)
+* Coming Soon 
 */
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
